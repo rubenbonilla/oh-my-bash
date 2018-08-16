c68ec56b182be4e8f7d60b1e9dc2b15f28365463
Ruben Bonilla Martinez <rbonilla@qdqmedia.com>
2018-08-16 08:12:59 +0200

    Removed custom folder from .gitignore

---
+++ custom/aliases/aliases.sh
@@ -0,0 +1,4 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+alias ssh='ssh -v -o TCPKeepAlive=yes -o ServerAliveInterval=50'
+alias robo3t='nohup robo3t > /dev/null 2>&1 &'
---
+++ custom/config.sh
@@ -0,0 +1,17 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/workspace/projects/m2/brainstormr
+# cd $brainstormr
+#
+
+# VIRTUALENVWRAPPER CONFIG
+WORKON_HOME=$HOME/workdir/.virtualenvs
+PROJECT_HOME=$HOME/workdir/projects
+VIRTUALENVWRAPPER_SCRIPT=$HOME/.local/bin/virtualenvwrapper.sh
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
+source $HOME/.local/bin/virtualenvwrapper_lazy.sh 
